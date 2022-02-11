b4467596d630fec7fb8e273f292d5b71a30a6110
Christoph Ruhee <Christoph.Ruhe@web.de>
2022-02-11 12:54:33 +0100

    Test 2-4

--- parking_lot/src/de/volkswagen/application/ParkingLot.java
+++ parking_lot/src/de/volkswagen/application/ParkingLot.java
@@ -1,15 +1,43 @@
 package de.volkswagen.application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
 
 	private int sizeParkingLot = 0;
+	private List<Integer> parkingLots = new ArrayList<>();
 
 	public ParkingLot(int sizeParkingLot) {
-		this.sizeParkingLot = sizeParkingLot;
+		if (sizeParkingLot >= 500) {
+			this.sizeParkingLot = 500;
+		}else {
+			this.sizeParkingLot = sizeParkingLot;
+		}
+		for (int i = 0; i < this.sizeParkingLot; i++) {
+			parkingLots.add(i + 1);
+		}
+
 	}
-	
+
 	public int getSizeParkingLot() {
 		return sizeParkingLot;
+	}
+
+	public int parkingFee(int hours, int hoursDailyFee) {
+		if (hours >= hoursDailyFee) {
+			return 30;
+		} else {
+			return hours * 3;
+		}
+	}
+
+	public int parkingLotNumber(int parkingLotNumber) {
+		if (parkingLotNumber < 1 || parkingLotNumber > sizeParkingLot) {
+			throw new RuntimeException("Parking Lot Number must between 1 and " + sizeParkingLot);
+		}
+		return parkingLots.get(parkingLotNumber - 1);
+
 	}
 
 }
--- parking_lot/src/de/volkswagen/tests/ParkingLotTest.java
+++ parking_lot/src/de/volkswagen/tests/ParkingLotTest.java
@@ -1,7 +1,8 @@
 package de.volkswagen.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,7 +10,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import de.volkswagen.application.ParkingLot;
 
 class ParkingLotTest {
-	
+
 //			Zero        : Zero case             : getSizeParkingLot() == 0
 //			One         : One case              : getSizeParkingLot() == 1
 //			Many        : Many case             : getSizeParkingLot() == 100
@@ -19,19 +20,75 @@ class ParkingLotTest {
 //			Simple      : Simple solutions      : Does my code make sense?
 
 	@ParameterizedTest
-	@ValueSource(ints = {0, 1, 100, Integer.MIN_VALUE, Integer.MAX_VALUE})
-	@DisplayName("getSizeParkingLotTest_returnsSizes")
+	@ValueSource(ints = { 0, 1, 100, Integer.MIN_VALUE, Integer.MAX_VALUE })
+	@DisplayName("getSizeParkingLotTest returnsSizes")
 	void getSizeParkingLotTest(int sizes) {
-		
+
 		// GIVEN A parking lot can hold up to 'n' cars at any given point in time.
 		ParkingLot parkingLot = new ParkingLot(sizes);
-		int expected = sizes;
-		
+		int expected = 0;
+		if (sizes >= 500) {
+			expected = 500;
+		} else {
+			expected = sizes;
+		}
+
 		// WHEN
 		int actual = parkingLot.getSizeParkingLot();
-		
+
 		// THEN
-		assertEquals(expected, actual);	
+		assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 0, 1, 100, Integer.MIN_VALUE, Integer.MAX_VALUE })
+	@DisplayName("getParkingFeeTest returnParkingFee")
+	void getParkingFeeTest(int hours) {
+
+		// GIVEN
+		// Parking in this lot has a per whole hour fee.
+		// Customers who spend less than 1 whole hour, are not charged anything.
+		// Parking in this lot has a daily fee based on a number of hours chosen by the
+		// owner.
+		// The daily fee would kick in after the prescribed number of whole hours has
+		// passed.
+		ParkingLot parkingLot = new ParkingLot(42);
+		int expected = 0;
+		if (hours >= 8) {
+			expected = 30;
+		} else {
+			expected = hours * 3;
+		}
+
+		// WHEN
+		int actual = parkingLot.parkingFee(hours, 8);
+
+		// THEN
+		assertEquals(expected, actual);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 0, 1, 100, Integer.MAX_VALUE })
+	@DisplayName("Foo")
+	void foo(int parkingLotNumber) {
+
+		// GIVEN
+		// Parking spaces are numbered from 1 to the maximum number of spaces on the lot
+		ParkingLot parkingLot = new ParkingLot(100);
+		int expected = parkingLotNumber;
+		// WHEN
+
+		// THEN
+		if (parkingLotNumber > 0 && parkingLot.getSizeParkingLot() >= parkingLotNumber) {
+			int actual = parkingLot.parkingLotNumber(parkingLotNumber);
+			assertEquals(expected, actual);
+		} else {
+			RuntimeException expectedException = Assertions.assertThrows(RuntimeException.class, () -> {
+				parkingLot.parkingLotNumber(parkingLotNumber);
+			}, "RuntimeException Error was expected");
+			assertEquals("Parking Lot Number must between 1 and " + parkingLot.getSizeParkingLot(),
+					expectedException.getMessage());
+		}
 	}
 
 }
